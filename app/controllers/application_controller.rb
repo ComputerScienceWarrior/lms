e6fa040d0baca67fc5a705ae8b5e85dd7da22cf4
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-10-19 11:53:11 -0700

    Update application_controller.rb to include sessions. Create skeleton of models for student, teacher and course.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,10 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
-
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
+  register Sinatra::ActiveRecordExtension
+  enable :sessions
+  set :session_secret, "password_security"
+  set :views, Proc.new { File.join(root, "../views/") }
 
   get "/" do
     erb :welcome
