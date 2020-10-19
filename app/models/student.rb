4254716be1b0f9406a4221cac843eb401f57469e
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-10-19 12:14:44 -0700

    Add slug functionality to student and validation for student username.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,13 @@
 class Student < ActiveRecord::Base
     has_many :courses
     belongs_to :course
+    validates :username, presence: true, uniqueness: true
+
+    def slug
+        self.username.gsub(" ", "-").downcase
+    end
+
+    def self.find_by_slug(slug)
+        User.all.find{ |user| user.slug == slug }
+    end
 end
