7e0734da0141d0d757d0cd86c7134155b6831de8
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-10-22 08:28:04 -0700

    Update course controller to create a @course with the logged in Student to associate them. Populate /courses/index.erb to show all courses from all users. Create courses show page for specific course information.

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -7,14 +7,18 @@ class CourseController < ApplicationController
     end
   
     get "/courses" do
+        @student = Student.find_by_id(session[:student_id])
         erb :"/courses/index"
     end
 
     get "/courses/create" do 
+        @student = Student.find_by_id(session[:student_id])
         erb :"/courses/new"
     end
 
     post "/courses" do
+        @student = Student.find_by_id(session[:student_id])
+        @course = Course.create(title: params[:title], cirriculum: params[:cirriculum], difficulty: params[:difficulty], language: params[:language], student_id: @student.id)
         erb :"/courses/show"
     end
 
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,13 @@
 class Course < ActiveRecord::Base
     belongs_to :student
     has_many :courses
+    validates :title, presence: true, uniqueness: true
+
+    def slug
+        self.title.gsub(" ", "-").downcase
+    end
+
+    def self.find_by_slug(slug)
+        Course.all.find{ |course| course.slug == slug }
+    end
 end
