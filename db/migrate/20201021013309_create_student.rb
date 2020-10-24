32b3cf9004cdf4ed3d4f98737f1f4647554d2b3a
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-10-23 18:08:23 -0700

    Create another student and run tests on login. Update /students/new.erb page styling. Create javascript code file for new user view to test input validation.

--- db/migrate/20201021013309_create_student.rb
+++ db/migrate/20201021013309_create_student.rb
@@ -5,7 +5,6 @@ class CreateStudent < ActiveRecord::Migration
       s.string :lastname
       s.string :username
       s.string :password_digest
-      s.integer :course_id
     end
   end
 end
---
+++ public/javascripts/students/new.js
@@ -0,0 +1,7 @@
+const fname = document.querySelector('#first-name')
+const lname = document.querySelector('#last-name')
+const uname = document.querySelector('#username')
+const password = document.querySelector('#password')
+const passwordConf = document.querySelector('#password-conf')
+var counter = 0
+
