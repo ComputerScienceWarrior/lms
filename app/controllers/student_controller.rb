b3741e42837342d3b75d64ecfc36b1485eb28602
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-10-30 21:04:46 -0700

    Add 3 new Helpers: invalid_credentials?, empty_fields?, and passwords_dont_match? to help with preventing bad characters in user fields. Remove settings route from users. Update '/post login to include use of Helpers methods.

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -15,22 +15,16 @@ class StudentController < ApplicationController
     end
 
     post "/signup" do
-        #ensure users can't add whatever characters they want
-        badChars = ["&", ",", ".", "/", "@", "#", "$", "%", "*", "(", ")"]
-        badChars.each do |bad_char|
-            if params[:firstname].include?(bad_char) || params[:lastname].include?(bad_char) || params[:username].include?(bad_char)
-                redirect to "/signup"
-            end
-        end
+        invalid_chars = Helpers.invalid_credentials?(params) 
+        empty_fields = Helpers.empty_fields?(params)
+        passwords_dont_match = Helpers.passwords_dont_match?(params[:password], params[:passwordconf])
 
-        #if the values are not empty and if passwords match, create user and show the page.
-        if ((params[:firstname] != "") && (params[:lastname] != "") && (params[:username] != "") && (params[:password] != "") && params[:password] == params[:passwordconf])
-            @student = Student.create(firstname: params[:firstname].strip, lastname: params[:lastname].strip, username: params[:username].strip, password: params[:password].strip)
-            session[:student_id] = @student.id
-            erb :"/students/show"
-        #otherwise, return user to create new user page again without making user.
+        if (invalid_chars || empty_fields || passwords_dont_match)
+            redirect '/signup'
         else
-            redirect to "/signup"
+            @student = Student.create(firstname: params[:firstname].strip, lastname: params[:lastname].strip, username: params[:username].strip, password: params[:password].strip)
+            session[:student_id] = @student.id #set student to logged in state
+            redirect "/students/show"
         end
     end
 
@@ -58,6 +52,11 @@ class StudentController < ApplicationController
         erb :"/students/show"
     end
 
+    get "/students/:id" do
+        @student = Student.find_by_id(session[:student_id])
+        erb :"/students/show"
+    end
+
     get "/students/:slug/edit" do
         @student = Student.find_by_id(session[:student_id])
         erb :"/students/edit"
@@ -66,7 +65,7 @@ class StudentController < ApplicationController
     patch "/students/:slug" do
         @student = Student.find_by_id(session[:student_id])
         @student.update(firstname: params[:firstname], lastname: params[:lastname], username: params[:username])
-        erb :"/students/show"
+        redirect "/students/show"
     end
 
     delete "/students/:slug" do
@@ -79,10 +78,5 @@ class StudentController < ApplicationController
         @student.delete #now, delete the student
         session.clear
         redirect to '/'
-    end
-
-    get "/settings" do
-        @student = Student.find_by_id(session[:student_id])
-        erb :"/students/settings"
     end
 end
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -6,4 +6,24 @@ class Helpers
     def self.is_logged_in?(session)
         !!session[:student_id]
     end
+
+    #takes in params hash and the route...used for all areas of program to prevent bad login information
+    def self.invalid_credentials?(params)
+        badChars = ["&", ",", ".", "/", "@", "#", "$", "%", "*", "(", ")", "!", "^", "_", "\"", ":", ";", "=", "+"]
+        badChars.each do |bad_char|
+            binding.pry
+            if ( (params[:firstname].include?(bad_char)) || (params[:lastname].include?(bad_char)) || (params[:username].include?(bad_char)) )
+                return true
+            end
+        end
+        return false
+    end
+
+    def self.empty_fields?(params)
+        params[:firstname] == "" || params[:lastname] == "" || params[:username] == "" || params[:password] == "" || params[:passwordconf] == "" ? true : false
+    end
+
+    def self.passwords_dont_match?(password, password_conf)
+        password != password_conf ? true : false
+    end
 end
